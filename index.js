e0f587a896d6e15d170a6ab8ef238626362561f6
Ionică Bizău <bizauionica@yahoo.com>
2014-07-13 21:14:45 +0300

    Added project files

---
+++ index.js
@@ -0,0 +1,61 @@
+// Dependencies
+var Mongo = require("mongodb")
+  , MongoClient = Mongo.MongoClient
+  , Mustache = require("mustache")
+  , Fs = require("fs")
+  ;
+
+const MONGO_URI = "mongodb://localhost:27017/baccalaureate"
+var template = Fs.readFileSync("./template").toString();
+var surnames = [
+    "Banciu",
+    "Bizău",
+    "Bogan",
+    "Bogdan",
+    "Bota",
+    "Cîrle",
+    "Dagău",
+    "Drăgoi",
+    "Turle",
+    "Matiu",
+    "Gurscă",
+    "Vintilă",
+    "Pantiș",
+    "Miklo",
+    "Mărginean",
+    "Gruie",
+    "Ile",
+    "Țirban",
+    "Jurca",
+    "Bulc",
+    "Hălbac"
+];
+
+MongoClient.connect(MONGO_URI, function(err, db) {
+    if (err) { throw err; }
+
+    var finalData = [];
+    var complete = 0;
+
+    for (var i = 0; i < surnames.length; ++i) {
+        (function (cS) {
+            db.collection("students").findOne({
+                "specialization" : "Matematica-Informatica",
+                "name": new RegExp(cS, "i")
+            }, function (err, obj) {
+                if (err) { throw err; }
+                finalData.push(obj);
+                if (++complete === surnames.length) {
+                    finalData.sort(function (a, b) {
+                        return a.name > b.name ? 1 : -1;
+                    });
+                    debugger
+                    console.log(Mustache.render(template, {
+                        results: finalData
+                    }));
+                    db.close();
+                }
+            });
+        })(surnames[i]);
+    }
+});
